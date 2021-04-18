48aa842e766d7f2be3601f0c80ebbce795c4eff1
rktejesh <73354337+rktejesh@users.noreply.github.com>
2021-04-18 07:07:39 +0530

    eventpages update

--- festomania/lib/src/views/ui/CodingEvent.dart
+++ festomania/lib/src/views/ui/CodingEvent.dart
@@ -1,25 +1,8 @@
-import 'package:festomania/src/views/ui/config.dart';
+import 'package:festomania/src/views/ui/EventPage.dart';
 import 'package:festomania/src/views/utils/search.dart';
 import 'package:flutter/material.dart';
-
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: CodingEventPage(),
-    );
-  }
-}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class CodingEventPage extends StatefulWidget {
   @override
@@ -59,25 +42,167 @@ class _CodingEventPageState extends State<CodingEventPage> {
                       Expanded(
                           child: SizedBox(
                             height: 200.0,
-                            child: ListView.builder(
-                              itemCount: CodingEventImages.length,
-                              itemBuilder: (BuildContext context, int index) {
-                                return Container(
-                                  padding:
-                                  EdgeInsets.only(bottom: 20, left: 20, right: 20),
-                                  height: 200,
-                                  width: double.infinity,
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      image: DecorationImage(
-                                          image: AssetImage(CodingEventImages[index]),
-                                          fit: BoxFit.fill),
-                                      borderRadius: const BorderRadius.all(
-                                        Radius.circular(25.0),
-                                      ),
-                                    ),
-                                  ),
-                                );
+                            child: StreamBuilder(
+                              stream: FirebaseFirestore.instance.collection('Events').snapshots(),
+                              builder: (context, eventSnapshot) {
+                                return eventSnapshot.hasData
+                                    ? ListView.builder(
+                                    itemCount: eventSnapshot.data.docs.length,
+                                    itemBuilder: (context, index) {
+                                      DocumentSnapshot eventData =
+                                      eventSnapshot.data.docs[index];
+                                      return (eventData
+                                          .data()['eventCategory']
+                                          .toString() ==
+                                          "Coding Events")
+                                          ? Container(
+                                        padding: EdgeInsets.only(
+                                            bottom: 20,
+                                            left: 20,
+                                            right: 20),
+                                        height: 300,
+                                        width: double.infinity,
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            boxShadow: [
+                                              BoxShadow(
+                                                color:
+                                                Color.fromRGBO(
+                                                    149,
+                                                    157,
+                                                    165,
+                                                    0.1),
+                                                offset:
+                                                Offset(0, 0),
+                                                blurRadius: 24,
+                                              )
+                                            ],
+                                          ),
+                                          child: Column(
+                                            children: [
+                                              InkWell(
+                                                child: Container(
+                                                  height: 200,
+                                                  decoration: BoxDecoration(
+                                                    boxShadow: [
+                                                      BoxShadow(
+                                                        color:
+                                                        Color.fromRGBO(
+                                                            149,
+                                                            157,
+                                                            165,
+                                                            0.1),
+                                                        offset:
+                                                        Offset(0, 0),
+                                                        blurRadius: 24,
+                                                      )
+                                                    ],
+                                                    image: DecorationImage(
+                                                        image: NetworkImage(
+                                                            eventData
+                                                                .data()[
+                                                            'imageLink']
+                                                                .toString()),
+                                                        fit: BoxFit.fill),
+                                                    borderRadius:
+                                                    const BorderRadius
+                                                        .only(
+                                                      topRight:
+                                                      Radius.circular(
+                                                          15.0),
+                                                      topLeft:
+                                                      Radius.circular(
+                                                          15.0),
+                                                    ),
+                                                  ),
+                                                ),
+                                                onTap: () {
+                                                  Navigator.of(context)
+                                                      .push(
+                                                      MaterialPageRoute(
+                                                        builder: (context) =>
+                                                            EventPage(eventData
+                                                                .reference.id),
+                                                      ));
+                                                },
+                                              ),
+                                              Padding(
+                                                padding: const EdgeInsets.all(15.0),
+                                                child: Container(
+                                                  decoration: BoxDecoration(
+                                                    boxShadow: [
+                                                      BoxShadow(
+                                                        color: Color.fromRGBO(
+                                                            149,
+                                                            157,
+                                                            165,
+                                                            0.1),
+                                                        offset: Offset(0, 0),
+                                                        blurRadius: 24,
+                                                      )
+                                                    ],
+                                                    borderRadius:
+                                                    const BorderRadius
+                                                        .only(
+                                                      topRight:
+                                                      Radius.circular(
+                                                          15.0),
+                                                      topLeft:
+                                                      Radius.circular(
+                                                          15.0),
+                                                    ),
+                                                  ),
+                                                  child: Row(
+                                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                                    children: [
+                                                      Column(
+                                                        children: [
+                                                          Text(eventData
+                                                              .data()[
+                                                          'eventName']
+                                                              .toString()),
+                                                          Text(eventData
+                                                              .data()[
+                                                          'collegeName']
+                                                              .toString()),
+                                                        ],
+                                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                                      ),
+                                                      TextButton(
+                                                        onPressed: () {
+                                                          FirebaseFirestore
+                                                              .instance
+                                                              .collection(
+                                                              'Users')
+                                                              .doc(FirebaseAuth
+                                                              .instance
+                                                              .currentUser
+                                                              .uid)
+                                                              .update({
+                                                            "saved": FieldValue
+                                                                .arrayUnion([
+                                                              eventData
+                                                                  .reference
+                                                                  .id
+                                                            ])
+                                                          });
+                                                        },
+                                                        child: Icon(
+                                                          Icons.bookmark,
+                                                          color: Colors.black,
+                                                        ),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      )
+                                          : Container();
+                                    })
+                                    : Loading();
                               },
                             ),
                           )
