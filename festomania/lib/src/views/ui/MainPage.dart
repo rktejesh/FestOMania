8c8d147f2d6a0daae62213b2d7ae8a6951be6b0f
sanskruti_shahu <lcs2020056@iiitl.ac.in>
2021-04-18 09:11:47 +0530

    updated changes

--- festomania/lib/src/views/ui/MainPage.dart
+++ festomania/lib/src/views/ui/MainPage.dart
@@ -1,15 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:fest_o_mania/src/views/utils/loading.dart';
 import 'package:festomania/src/views/utils/loading.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
-import 'package:fest_o_mania/src/views/utils/AppDrawer.dart';
+import 'package:festomania/src/views/utils/AppDrawer.dart';
 import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:fest_o_mania/src/views/utils/search.dart';
-import 'package:fest_o_mania/src/views/ui/EventPage.dart';
+import 'package:festomania/src/views/utils/search.dart';
+import 'package:festomania/src/views/ui/EventPage.dart';
 import 'package:sliver_tools/sliver_tools.dart';
 
 class MainPageUpcoming extends StatefulWidget {
